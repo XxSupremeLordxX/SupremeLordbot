b72f7285887158e5f62bdcfa95bc3f659b3c70a7
Ishwar Govind <65118423+XxSupremeLordxX@users.noreply.github.com>
2020-06-16 17:41:20 +0530

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require_relative '../SupremeLordbot/bot.rb'
-require_relative '../SupremeLordbot/motivate.rb'
+require_relative './bot.rb'
+require_relative './motivate.rb'
 #require_relative '../SupremeLordbot/memes.rb'
 Bot.new
